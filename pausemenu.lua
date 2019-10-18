b4d19022e773279834fa8c1e2c366e3f9c24b303
NotGarfield <36775882+GarfieldeuhV1@users.noreply.github.com>
2019-10-18 16:41:03 +0200

    Add files via upload

---
+++ pausemenu.lua
@@ -0,0 +1,23 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+local id = PlayerId()
+local serverID = GetPlayerServerId(PlayerId())
+players = {}
+for i = 0, 255 do
+    if NetworkIsPlayerActive( i ) then
+        table.insert( players, i )
+    end
+end
+Citizen.CreateThread(function()
+  AddTextEntry('FE_THDR_GTAO', '~f~Bienvenue, vous êtes sur **server name** ~w~-~g~ Votre ID est le ' .. GetPlayerServerId(PlayerId()) .. ' ~u~')
+  AddTextEntry('PM_PANE_LEAVE', '~o~Retourner sur la liste des serveurs.')
+  AddTextEntry('PM_PANE_QUIT', '~r~Quitter cette belle ville :c')
+  AddTextEntry('PM_SCR_MAP', '~b~Carte de Los Santos')
+  AddTextEntry('PM_SCR_GAM', '~r~Prendre l\'avion')
+  AddTextEntry('PM_SCR_INF', '~g~Logs')
+  AddTextEntry('PM_SCR_SET', '~p~Configuration')
+  AddTextEntry('PM_SCR_STA', '~f~Statistiques')
+  AddTextEntry('PM_SCR_RPL', '~y~Éditeur ∑')
+end)
