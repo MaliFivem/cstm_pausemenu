daaea73e7781e2f26ae5cbd992d3ba222cf9fea4
NotGarfield <36775882+GarfieldeuhV1@users.noreply.github.com>
2019-10-19 18:56:59 +0200

    Update pausemenu.lua

--- pausemenu.lua
+++ pausemenu.lua
@@ -2,16 +2,19 @@ function AddTextEntry(key, value)
 	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
 
-local id = PlayerId()
-local serverID = GetPlayerServerId(PlayerId())
-players = {}
-for i = 0, 255 do
-    if NetworkIsPlayerActive( i ) then
-        table.insert( players, i )
-    end
-end
+-- Uncomment if you want add Players Steam or connected players
+
+--local id = PlayerId()
+--local serverID = GetPlayerServerId(PlayerId())
+--players = {}
+--for i = 0, 255 do
+ --   if NetworkIsPlayerActive( i ) then
+ --       table.insert( players, i )
+ --   end
+--end
+
 Citizen.CreateThread(function()
-  AddTextEntry('FE_THDR_GTAO', '~f~Bienvenue, vous êtes sur **server name** ~w~-~g~ Votre ID est le ' .. GetPlayerServerId(PlayerId()) .. ' ~u~')
+  AddTextEntry('FE_THDR_GTAO', '~f~Bienvenue, vous êtes sur **server name**.')
   AddTextEntry('PM_PANE_LEAVE', '~o~Retourner sur la liste des serveurs.')
   AddTextEntry('PM_PANE_QUIT', '~r~Quitter cette belle ville :c')
   AddTextEntry('PM_SCR_MAP', '~b~Carte de Los Santos')
